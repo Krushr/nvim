8236ffd6b2b81b550389f16e65f2500b8c5e9059
Adam Hardiman <adam@bitrusher.com>
2025-02-24 11:29:16 +1000

    scope(colorscheme): back to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,16 @@
 return {
   -- Configure LazyVim to load tokyonight
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight-night",
